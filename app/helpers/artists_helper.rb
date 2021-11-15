c001acb5b5a8a903f8790cc0d3fa97b970041bca
essbee808 <sheena.myb@gmail.com>
2021-11-14 19:54:41 -0800

    Create conditional statement in artists helper

--- app/helpers/artists_helper.rb
+++ app/helpers/artists_helper.rb
@@ -1,3 +1,14 @@
 module ArtistsHelper
     
+    def display_artist(song)
+        #if an artist is already associated with the song
+        #return a link to the artist's show page
+        #binding.pry
+        if song.artist && song.artist.name != ""
+            link_to song.artist.name, artist_path(song.artist)
+        else
+            link_to "Add Artist", edit_song_path(song)
+        end
+    end
+
 end
--- app/helpers/songs_helper.rb
+++ app/helpers/songs_helper.rb
@@ -1,13 +1,3 @@
 module SongsHelper
 
-    def display_artist
-        #if an artist is already associated with the song
-        #return a link to the artist's show page
-        if song.artist
-           redirect_to :show
-        else
-           render :edit 
-        end
-    end
-
 end
